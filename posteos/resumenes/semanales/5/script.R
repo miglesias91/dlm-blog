ecf1c0e2f7d6f9f1e6d6b3fd79bf2df6251d6da5
manu <m.iglesias91@gmail.com>
2021-02-10 13:28:18 -0300

    refactor script semana5

--- posteos/resumenes/semanales/5/script.R
+++ posteos/resumenes/semanales/5/script.R
@@ -40,41 +40,5 @@ dfoto_palabras_titulo = foto_palabras(que = c('terminos','personas'),
 setcolorder(dfoto_palabras_titulo, c('palabra', 'freq','por_noticia', 'diario'))
 fwrite(dfoto_palabras_titulo[diario != 'casarosada'][1:200], '~/repos/dicenlosmedios.com.ar/resumen-semanal-5/palabras_titulo.csv')
 
-dfoto_palabras_titulo22 = fread('~/repos/dicenlosmedios.com.ar/resumen-semanal-5/palabras_titulo.csv')
+dfoto_palabras_titulo = fread('~/repos/dicenlosmedios.com.ar/resumen-semanal-5/palabras_titulo.csv')
 dfoto_palabras_texto = fread('~/repos/dicenlosmedios.com.ar/resumen-semanal-5/palabras_texto.csv')
-
-a_dibujar = rbind(
-  dfoto_palabras_titulo[palabra %in% c('Alberto', 'Alberto Fernández'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Alberto Fernández'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Alberto', 'Alberto Fernández'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Alberto Fernández'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('Larreta', 'Horacio Larreta', 'Horacio Rodríguez Larreta'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Larreta'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Larreta', 'Horacio Larreta', 'Horacio Rodríguez Larreta'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Larreta'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('Gildo Insfrán', 'Insfrán'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Insfrán'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Gildo Insfrán', 'Insfrán'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Insfrán'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('Kicillof', 'Axel Kicillof'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Kicillof'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Kicillof', 'Axel Kicillof'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Kicillof'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('Macri', 'Mauricio Macri'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Macri'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Macri', 'Mauricio Macri'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Macri'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('CFK', 'Cristina', 'Cristina Fernández de Kirchner'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'CFK'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('CFK', 'Cristina', 'Cristina Fernández de Kirchner'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'CFK'), keyby=.('clave' = diario)]
-)
-gglollipop(a_dibujar[clave != 'casarosada'])
-
-a_dibujar = rbind(
-  dfoto_palabras_titulo[palabra %in% c('Boca', 'Boca Juniors', 'Riquelme'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Boca'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Boca', 'Boca Juniors', 'Riquelme'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Boca'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('River', 'River Plate', 'Gallardo'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'River'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('River', 'River Plate', 'Gallardo'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'River'), keyby=.('clave' = diario)]
-  )
-gglollipop(a_dibujar[clave != 'casarosada'], colores = c('darkblue', 'red'))
-
-a_dibujar = rbind(
-  dfoto_palabras_titulo[palabra %in% c('vacunación'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'vacunación'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('vacunación'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'vacunación'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Sputnik', 'Sputnik V'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Sputnik V'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('Sputnik', 'Sputnik V'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Sputnik V'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('AstraZeneca'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'AstraZeneca'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('AstraZeneca'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'AstraZeneca'), keyby=.('clave' = diario)],
-  dfoto_palabras_texto[palabra %in% c('Pfizer'), .('valor' = sum(por_noticia), 'grupo' = 'texto', 'grupo2' = 'Pfizer'), keyby=.('clave' = diario)],
-  dfoto_palabras_titulo[palabra %in% c('Pfizer'), .('valor' = sum(por_noticia), 'grupo' = 'titulo', 'grupo2' = 'Pfizer'), keyby=.('clave' = diario)]
-)
-gglollipop(a_dibujar[clave != 'casarosada'])
